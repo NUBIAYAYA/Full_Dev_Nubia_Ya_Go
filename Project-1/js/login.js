f16f81146eed805dd4c7f2362a5718e93cbd611c
NubiaY <nubia.yaya@gmail.com>
2024-08-05 21:44:01 -0500

    Plis

--- Project-1/js/login.js
+++ Project-1/js/login.js
@@ -1,4 +1,4 @@
-
+console.log ('inicio sesion');
 
 const usuario = 'admin'
 const clave = '1234'
@@ -6,18 +6,20 @@ let contador =0
 
 
 let  usuarios = [
-    { usuario: 'usuario1', clave: '1234' },
-    { usuario: 'usuario2', clave: '5678' },
-    { usuario: 'usuario3', clave: '9101' },
-    { usuario: 'usuario4', clave: '1121' },
-    { usuario: 'usuario5', clave: '3141' },
-];
+    { "usuario": "usuario1", "clave": "1234" },
+    { "usuario": "usuario2", "clave": "5678" },
+    { "usuario": "usuario3", "clave": "9101" },
+    { "usuario": "usuario4", "clave": "1121" },
+    { "usuario": "usuario5", "clave": "3141" }
+]
 
 function pruebaBoton(){
-    let user = document.getElementById('user').value
-    let buscarUsuario= usuarios.find(usuario=> usuario.usuario===user)
+    let user = document.getElementById('user').value.toLocaleUpperCase()
+    let pw = document.getElementById('pw').value
+
+    let usuarioLogueado=usuarios.some(usuario =>usuario.usuario.toLocaleUpperCase()==user&&usuario.clave==pw)
 
 }
-let usuarioLogueado=usuarios.some(usuario=>usuario.usuario.toLocaleUpperCase()==user && usuario.clave==pw)
+
 
 console.log (usuarioLogueado);
