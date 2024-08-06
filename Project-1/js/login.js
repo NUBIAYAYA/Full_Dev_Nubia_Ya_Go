ac993251ec21ffbaa20d7fb8fa07ea12adc48818
NubiaY <nubia.yaya@gmail.com>
2024-08-05 20:47:43 -0500

    Prueba7

--- Project-1/js/login.js
+++ Project-1/js/login.js
@@ -3,14 +3,14 @@
 const usuario = 'admin'
 const clave = '1234'
 let contador =0
-let intentosFallidos = {};
+
 
 let  usuarios = [
-    { id: 1, usuario: 'usuario1', clave: '1234' },
-    { id: 2, usuario: 'usuario2', clave: '5678' },
-    { id: 3, usuario: 'usuario3', clave: '9101' },
-    { id: 4, usuario: 'usuario4', clave: '1121' },
-    { id: 5, usuario: 'usuario5', clave: '3141' }
+    { usuario: 'usuario1', clave: '1234' },
+    { usuario: 'usuario2', clave: '5678' },
+    { usuario: 'usuario3', clave: '9101' },
+    { usuario: 'usuario4', clave: '1121' },
+    { usuario: 'usuario5', clave: '3141' },
 ];
 
 function pruebaBoton(){
